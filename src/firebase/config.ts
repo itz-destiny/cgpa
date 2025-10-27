20d6fef394ee428e1e9cf8643524aa5d873419d8
Dynamic Debugger <debuggerdynamic@gmail.com>
2025-10-27 16:07:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5197133638-a5ba9",
+  "appId": "1:92165286203:web:8e4de2a3441366392caf1e",
+  "apiKey": "AIzaSyDfqGhKBJ0tg6PE69kfGyWw4Iem_stHvMA",
+  "authDomain": "studio-5197133638-a5ba9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "92165286203"
+};
